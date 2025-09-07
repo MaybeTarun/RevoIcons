381f64271feb6f8839794f2114b011e46ad72970
MaybeTarun <tarunthegreat2003@gmail.com>
2025-09-07 21:16:27 +0530

    still testing

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Export all icons
 export { default as Icon1 } from "./icons/Icon1.jsx";
-export { default as Icon2 } from "./icons/Icon2.js";
+export { default as Icon2 } from "./icons/Icon2.jsx";
 
 // Default export with all icons
 import Icon1 from "./icons/Icon1.jsx"
-import Icon2 from "./icons/Icon2.js"
+import Icon2 from "./icons/Icon2.jsx"
 
 export default {
   Icon1,
