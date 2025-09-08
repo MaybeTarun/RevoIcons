50388dd8d2e092dc4faccf4ad40affd6afe051de
MaybeTarun <tarunthegreat2003@gmail.com>
2025-09-08 11:39:16 +0530

    idek

---
+++ icons/Template.jsx
@@ -0,0 +1,22 @@
+const Revo = ({ size = 24, color = "currentColor", ...props }) => (
+  <svg
+    width="500"
+    height="500"
+    viewBox="0 0 500 500"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g clipPath="url(#clip0_2008_57)">
+      <circle cx="250" cy="250" r="225" fill="#EFF0EF" stroke="#2D2A32" strokeWidth="50" />
+      <path d="M247 347L362.5 222.5M362.5 222.5H265M362.5 222.5V317.5" stroke="#2D2A32" strokeWidth="50" strokeLinecap="round" strokeLinejoin="round" />
+    </g>
+    <defs>
+      <clipPath id="clip0_2008_57">
+        <rect width="500" height="500" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+)
+
+export default Revo
--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// Export all icons
-export { default as Icon1 } from "./icons/Icon1.jsx";
-export { default as Icon2 } from "./icons/Icon2.jsx";
+export { default as Revo } from "./icons/IconTemplate.jsx";
 
-// Default export with all icons
-import Icon1 from "./icons/Icon1.jsx"
-import Icon2 from "./icons/Icon2.jsx"
+import Revo from "./icons/IconTemplate.jsx"
 
 export default {
-  Icon1,
-  Icon2,
+  Revo,
 }
