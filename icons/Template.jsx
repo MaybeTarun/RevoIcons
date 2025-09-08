60aa8771f86da8ae749bf6bb8e35474d21b995d7
MaybeTarun <tarunthegreat2003@gmail.com>
2025-09-08 11:47:15 +0530

    hope it works now

--- icons/Template.jsx
+++ icons/Template.jsx
@@ -1,15 +1,16 @@
 const Revo = ({ size = 24, color = "currentColor", ...props }) => (
   <svg
-    width="500"
-    height="500"
+    width={size}
+    height={size}
     viewBox="0 0 500 500"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    color={color}
     {...props}
   >
     <g clipPath="url(#clip0_2008_57)">
-      <circle cx="250" cy="250" r="225" fill="#EFF0EF" stroke="#2D2A32" strokeWidth="50" />
-      <path d="M247 347L362.5 222.5M362.5 222.5H265M362.5 222.5V317.5" stroke="#2D2A32" strokeWidth="50" strokeLinecap="round" strokeLinejoin="round" />
+      <circle cx="250" cy="250" r="225" fill="none" stroke="currentColor" strokeWidth="50" />
+      <path d="M247 347L362.5 222.5M362.5 222.5H265M362.5 222.5V317.5" stroke="currentColor" strokeWidth="50" strokeLinecap="round" strokeLinejoin="round" />
     </g>
     <defs>
       <clipPath id="clip0_2008_57">
--- index.d.ts
+++ index.d.ts
@@ -6,12 +6,10 @@ export interface IconProps extends SVGProps<SVGSVGElement> {
   color?: string
 }
 
-export declare const Icon1: React.FC<IconProps>
-export declare const Icon2: React.FC<IconProps>
+export declare const Revo: React.FC<IconProps>
 
 declare const _default: {
-  Icon1: React.FC<IconProps>
-  Icon2: React.FC<IconProps>
+  Revo: React.FC<IconProps>
 }
 
 export default _default
