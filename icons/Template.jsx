c717df5c96beff6438b25f7fc0b69b3e238282df
MaybeTarun <tarunthegreat2003@gmail.com>
2025-09-08 12:48:29 +0530

    new testing

--- icons/Template.jsx
+++ icons/Template.jsx
@@ -1,22 +1,17 @@
-const Revo = ({ size = 24, color = "currentColor", ...props }) => (
+const Revo = ({ size = 32, color = "currentColor", fill = "none", ...props }) => (
   <svg
     width={size}
     height={size}
-    viewBox="0 0 500 500"
+    viewBox="0 0 32 32"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     color={color}
     {...props}
   >
-    <g clipPath="url(#clip0_2008_57)">
-      <circle cx="250" cy="250" r="225" fill="none" stroke="currentColor" strokeWidth="50" />
+    <g>
+      <circle cx="250" cy="250" r="225" fill={fill} stroke="currentColor" strokeWidth="50" />
       <path d="M247 347L362.5 222.5M362.5 222.5H265M362.5 222.5V317.5" stroke="currentColor" strokeWidth="50" strokeLinecap="round" strokeLinejoin="round" />
     </g>
-    <defs>
-      <clipPath id="clip0_2008_57">
-        <rect width="500" height="500" fill="white" />
-      </clipPath>
-    </defs>
   </svg>
 )
 
--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,7 @@ import type { SVGProps } from "react"
 export interface IconProps extends SVGProps<SVGSVGElement> {
   size?: number | string
   color?: string
+  fill?: string
 }
 
 export declare const Revo: React.FC<IconProps>
