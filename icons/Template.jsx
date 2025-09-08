e15f62bbbd7c91b55b22cd8a2991224e8cf0045f
MaybeTarun <tarunthegreat2003@gmail.com>
2025-09-08 12:58:41 +0530

    huhu

--- icons/Template.jsx
+++ icons/Template.jsx
@@ -8,10 +8,15 @@ const Revo = ({ size = 32, color = "currentColor", fill = "none", ...props }) =>
     color={color}
     {...props}
   >
-    <g>
+    <g clipPath="url(#clip0_2008_57)">
       <circle cx="250" cy="250" r="225" fill={fill} stroke="currentColor" strokeWidth="50" />
       <path d="M247 347L362.5 222.5M362.5 222.5H265M362.5 222.5V317.5" stroke="currentColor" strokeWidth="50" strokeLinecap="round" strokeLinejoin="round" />
     </g>
+    <defs>
+      <clipPath id="clip0_2008_57">
+        <rect width="32" height="32" fill="white" />
+      </clipPath>
+    </defs>
   </svg>
 )
 
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 export { default as Revo } from "./icons/Template.jsx";
+export { default as Linkedin } from "./icons/Linkedin.jsx";
 
 import Revo from "./icons/Template.jsx"
+import Linkedin from "./icons/Linkedin.jsx"
 
 export default {
   Revo,
+  Linkedin
 }
